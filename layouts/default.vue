251abab9b30256b125b8a0aa065188be655535a6
anthony <luxingchaode@gmail.com>
2024-12-26 10:02:30 +0800

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <AppHeader />
+    <h4 class="bg-red-300">content in layout file</h4>
+    <slot />
+    <AppFooter />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Welcome to the about</h1>
+    <AppAlert>
+      This is an auto-imported component about
+    </AppAlert>
+  </div>
+</template>
