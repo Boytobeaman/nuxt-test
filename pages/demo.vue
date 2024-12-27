47ad7d89abe8bddca1ecd2f203cfbb59ab2eecf3
anthony <luxingchaode@gmail.com>
2024-12-27 09:41:56 +0800

    add demo page

---
+++ pages/demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage hhh</h1>
+    <div class="nav">
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/contact">Contact</NuxtLink>
+    </div>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
